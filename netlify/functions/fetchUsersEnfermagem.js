17bbe708ed4f742943185cd76c0647cbc2276c21
thixgor <throdrigf@gmail.com>
2025-02-28 23:55:58 -0300

    Create fetchUsersEnfermagem.js

---
+++ netlify/functions/fetchUsersEnfermagem.js
@@ -0,0 +1,23 @@
+// netlify/functions/fetchUsersPsicologia.js
+exports.handler = async (event, context) => {
+    try {
+        const targetUrl = 'https://pastebin.com/raw/XwUn8jgz';
+        const response = await fetch(targetUrl);
+
+        if (!response.ok) {
+            throw new Error('Erro ao carregar usuários');
+        }
+
+        const data = await response.text(); // Pega o conteúdo em formato de texto
+
+        return {
+            statusCode: 200,
+            body: data,
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Erro ao carregar usuários' }),
+        };
+    }
+};
