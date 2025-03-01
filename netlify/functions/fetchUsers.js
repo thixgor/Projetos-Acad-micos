e6c6baf34a6ea1c0c2a0b9058ac4735311927259
thixgor <throdrigf@gmail.com>
2025-03-01 00:10:16 -0300

    Update fetchUsers.js

--- netlify/functions/fetchUsers.js
+++ netlify/functions/fetchUsers.js
@@ -1,7 +1,29 @@
-// netlify/functions/fetchUsersMed.js
+// netlify/functions/fetchUsers.js
 exports.handler = async (event, context) => {
     try {
-        const targetUrl = 'https://pastebin.com/raw/zFYnPjcE';
+        // Mapeamento dos cursos e suas URLs
+        const cursos = {
+            medicina: 'https://pastebin.com/raw/zFYnPjcE',
+            biomedicina: 'https://pastebin.com/raw/gQGspCcf',
+            enfermagem: 'https://pastebin.com/raw/XwUn8jgz',
+            medvet: 'https://pastebin.com/raw/xEq858u6',
+            odonto: 'https://pastebin.com/raw/3G8HG9x2',
+            psicologia: 'https://pastebin.com/raw/MqGpECkQ',
+        };
+
+        // Obtém o curso da query string
+        const curso = event.queryStringParameters.curso;
+
+        // Verifica se o curso existe no mapeamento
+        if (!cursos[curso]) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ error: 'Curso inválido' }),
+            };
+        }
+
+        // Faz o fetch da URL correspondente ao curso
+        const targetUrl = cursos[curso];
         const response = await fetch(targetUrl);
 
         if (!response.ok) {
