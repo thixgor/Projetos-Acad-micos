2ec1772ab0a12bd0cce59f9e7aaa63a93f9c1f5a
thixgor <throdrigf@gmail.com>
2025-02-28 23:44:57 -0300

    Create fetchUsers.js

---
+++ netlify/functions/fetchUsers.js
@@ -0,0 +1,23 @@
+// netlify/functions/fetchUsers.js
+exports.handler = async (event, context) => {
+    try {
+        const targetUrl = 'https://pastebin.com/raw/zFYnPjcE';
+        const response = await fetch(targetUrl);
+
+        if (!response.ok) {
+            throw new Error('Erro ao carregar usuários');
+        }
+
+        const data = await response.text(); // Pega o conteúdo em formato de texto
+
+        return {
+            statusCode: 200,
+            body: data,
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Erro ao carregar usuários' }),
+        };
+    }
+};
